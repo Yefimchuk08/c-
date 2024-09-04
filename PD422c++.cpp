77407190a3a33f0ed1e04dfeb17b3fba95280534
Yefimchuk08 <dmytroyefimchuk08@gmail.com>
2024-09-04 17:25:17 +0300

    Add files via upload

---
+++ PD422c++.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+//    cout << "Enter a number: ";
+//    int num = 0;
+//    cin >> num;
+//    if (num%1==0 && num%num==0)
+//    {
+//        cout << "Easy num" << endl;
+//    }
+//    else
+//    {
+//        cout << "Not easy num" << endl;
+//    }
+
+	//int time;
+	//cout << "Enter time in s: ";
+	//cin >> time;
+	//cout << "Ostalos in h: " << 24 - (time / 60 / 60) << endl;
+	//cout << "Ostalos in hv:  " << (24 * 60) - (time / 60 ) << endl;
+	//cout << "Ostalos in s: " << (24 * 60 * 60) - time << endl;
+	//cout << "" << endl;
+	//cout << "Present time in h" << time * 60 * 60 << endl;
+	//cout << "Present time in hv" << time * 60 << endl;
+	//cout << "Present time in h" << time * 60 * 60 << endl;
+
+	//int d;
+	//cout << "Enter d: ";
+	//cin >> d;
+	//const float PI = 3.14;
+	//cout << "S = " << 0.25 * PI * d * d << endl;
+	//cout << "P = " << PI * d << endl;
+
+	//int a, b;
+	//cout << "Enter two numb: ";
+	//cin >> a;
+	//cin >> b;
+	//cout << "SUM: " << a + b << "\t" << "DOB: " << a * b << "\t" << "Ser AR: " << (float)(a + b) / 2 << endl;
+
+	//int a, b, c;
+	//cout << "Enter three numb: ";
+	//cin >> a;
+	//cin >> b;
+	//cin >> c;
+	//cout << "SUM: " << a + b + c << "\t" << "DOB: " << a * b * c << "\t" << "Ser AR: " << (float)(a + b + c) / 3 << endl;
+
+	//int price, count, znuzhka;
+	//cout << "Enter price: ";
+	//cin >> price;
+	//cout << "Enter count: ";
+	//cin >> count;
+	//cout << "Enter %: ";
+	//cin >> znuzhka;
+	//cout << "All price: " << ((price * count) / 100) * znuzhka << endl;
+
+	//int gb, v;
+	//cout << "Enter gb: ";
+	//cin >> gb;
+	//cout << "Enter v: ";
+	//cin >> v;
+	//cout << "Sec: " << (gb * 1024 * 1024 * 1024 * 8) / v;
+	//cout << "Min: " << ((gb * 1024 * 1024 * 1024 * 8) / v)*60;
+	//cout << "Hour: " << ((gb * 1024 * 1024 * 1024 * 8) / v) * 3600;
+}
