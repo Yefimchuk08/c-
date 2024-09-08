2705d1ec78a7b28174c93d88e73038b9c5a6e571
Yefimchuk08 <dmytroyefimchuk08@gmail.com>
2024-09-08 18:00:51 +0300

    Add files via upload

---
+++ FileName.cpp
@@ -0,0 +1,54 @@
+#include <iostream>;
+using namespace std;
+
+//int main() {
+	//cout << "Translate to eanglish" << endl;
+	//cout << "" << endl;
+	//cout << "        Silhouettes" << endl;
+	//cout << "" << endl;
+	//cout << "I looked into your eyes" << endl;
+	//cout << "Silhouettes say there's peace there" << endl;
+	//cout << "And I remembered all our nights" << endl;
+	//cout << "The silhouettes say it's peaceful there" << endl;
+	//cout << "" << endl;
+	//cout << "But I hope it's good" << endl;
+	//cout << "The silhouettes say it's peaceful" << endl;
+	//cout << "And everyone can still see the sky and the sea" << endl;
+	//cout << "The silhouettes say it's peaceful there" << endl;
+	//cout << "" << endl;
+	//cout << "SadSvit & HAPPINESS STRUCTURE" << endl;
+
+	//cout << "Every" << endl;
+	//cout << "Hunret" << endl;
+	//cout << "Want" << endl;
+	//cout << "To know" << endl;
+	//cout << "Where" << endl;
+	//cout << "Sits" << endl;
+	//cout << "Pheasant" << endl;
+
+
+//}
+int main() {
+//	cout << "  \"Hello, World!\"" << endl;
+//	cout << "/Bjarne Srtoustrup/" << endl;
+//
+//	cout << "  Hello, World!" << endl;
+//	cout << "\\\\Dennis Rithi\\\\" << endl;
+//
+//	cout << "To be, or not to be: that is the question:" << endl;
+//	cout << "Whether 'tis nobler in the mind to suffer" << endl;
+//	cout << "The slings and arrows of outrageous fortune," << endl;
+//	cout << "Or to take arms against a sea of troubles," << endl;
+//	cout << "And by opposing end them? " << endl;
+//	cout << "William Shakespeare" << endl;
+//
+//
+
+	//cout << "Land plots for sale" << endl;
+	//cout << "Location: Kyiv region. picturesque places, near a forest and a river. Ideal for building cottages or farming." << endl;
+	//cout << "Plor area: from 1 to 10 hectares." << endl;
+	//cout << "Infrastructyre:\n * Electicity is connected;\n * Asphalted entrance;\n * Gas networks nearby;\n * Internet access;" << endl;
+	//cout << "Ptice: from $1,000 per hectare, bargaining is possible when buing more than 5 hectares." << endl;
+	//cout << "Convenient transport connections:\n * 30 minutes from Kyiv; \n * An international track is nearby;" << endl;
+	//cout << "Contact information: \n Tel:333-33-33\n 333|333|\n 33|33|\n 33|33| \n etc." << endl;
+}
