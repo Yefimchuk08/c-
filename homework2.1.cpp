f21c2de738e08147019e098864c7c13e46705448
Yefimchuk08 <dmytroyefimchuk08@gmail.com>
2024-09-08 18:54:23 +0300

    Add files via upload

---
+++ homework2.1.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    //int a;
+    //cout << "Enter num: ";
+    //cin >> a;
+    //if (a % 2 == 0)
+    //{
+    //    cout << "Num is good" << endl;
+    //}
+    //else
+    //{
+    //    cout << "Num not good" << endl;
+    //}
+
+    //int a, b;
+    //cout << "Enter first num: ";
+    //cin >> a;
+    //cout << "Enter second num: ";
+    //cin >> b;
+    //if (a < b)
+    //{
+    //    cout << a << endl;
+    //}
+    //else
+    //{
+    //    cout << b << endl;
+    //}
+    //
+
+    //float a;
+    //cout << "Enter num: ";
+    //cin >> a;
+    //if (a < 0)
+    //{
+    //    cout << "Num is negative" << endl;
+    //}
+    //else if (a == 0)
+    //{
+    //    cout << "Num = 0" << endl;
+    //}
+    //else
+    //{
+    //    cout << "Num is positive" << endl;
+    //}
+
+    //float a, b;
+    //cout << "Enter first num: ";
+    //cin >> a;
+    //cout << "Enter second num: ";
+    //cin >> b;
+    //if (a == b)
+    //{
+    //    cout << "a=b" << endl;
+    //}
+    //else
+    //{
+    //    if (a > b)
+    //    {
+    //        cout << b << "  " << a << endl;
+    //    }
+    //    else
+    //    {
+    //        cout << a << "  " << b << endl;
+    //    }
+    //}
+
+    //int a, b, c, d, e;
+    //cout << "Enter first rating: ";
+    //cin >> a;
+    //cout << "Enter second rating: ";
+    //cin >> b; 
+    //cout << "Enter third rating: ";
+    //cin >> c; 
+    //cout << "Enter fourth rating: ";
+    //cin >> d; 
+    //cout << "Enter fiveth rating: ";
+    //cin >> e; 
+    //if (((a + b + c + d + e) / 5) < 4)
+    //{
+    //    cout << "Oups" << endl;
+    //}
+    //else
+    //{
+    //    cout << "Congratulation. You did it" << endl;
+    //}
+
+    //int a;
+    //cout << "Enter num: ";
+    //cin >> a;
+    //if (a % 2 == 0)
+    //{
+    //    cout << a * 3 << endl;
+    //}
+    //else
+    //{
+    //    cout << (float)a / 2 << endl;
+    //}
+
+    //int a, b, choise;
+    //cout << "Enter first num: ";
+    //cin >> a;
+    //cout << "Enter second num: ";
+    //cin >> b;
+    //cout << "1. +" << endl;
+    //cout << "2. -" << endl;
+    //cout << "3. *" << endl;
+    //cout << "4. /" << endl;
+    //cout << "What your choise: " << endl;
+    //cin >> choise;
+    //switch (choise)
+    //{
+    //case 1:
+    //    cout << "Sum : " << a + b << endl;
+    //case 2:
+    //    cout << "Subtraction: " << a - b << endl;
+    //case 3:
+    //    cout << "Multiplication: " << a * b << endl;
+    //case 4:
+    //    cout << "Division: " << (float)a / b << endl;
+    //default:
+    //    break;
+    //}
+}
+ 
