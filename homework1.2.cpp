fe60edbc74a445d43fd9000bf0912cd95ed285e5
Yefimchuk08 <dmytroyefimchuk08@gmail.com>
2024-09-08 18:04:49 +0300

    Update homework1.2.cpp

--- homework1.2.cpp
+++ homework1.2.cpp
@@ -4,6 +4,119 @@ using namespace std;
 int main()
 {
     
+    //int minutes_per_week = 7 * 24 * 60;
+    //int minutes_per_day = 24 * 60;      
+    //int seconds_per_minute = 60;  
+    //int total_minutes;
+    //cout << "Введіть кількість хвилин, що минули з початку тижня: ";
+    //cin >> total_minutes;
+    //int current_day = total_minutes / minutes_per_day;   
+    //int remaining_minutes = total_minutes % minutes_per_day; 
+    //int current_hour = remaining_minutes / 60; 
+    //int current_minute = remaining_minutes % 60;   
+    //int current_second = 0; 
+    //int remaining_time = minutes_per_week - total_minutes;
+    //int days_left = remaining_time / minutes_per_day;   
+    //int minutes_left = remaining_time % minutes_per_day; 
+    //int hours_left = minutes_left / 60;     
+    //int final_minutes_left = minutes_left % 60;   
+    //int seconds_left = final_minutes_left * seconds_per_minute;
+    //cout << "Поточний час: " << endl;
+    //cout << "День: " << current_day + 1 << endl;
+    //cout << "Година: " << current_hour << endl;
+    //cout << "Хвилини: " << current_minute << endl;
+    //cout << "Секунди: " << current_second << endl;
+    //cout << "До кінця тижня залишилося: " << endl;
+    //cout << "Днів: " << days_left << endl;
+    //cout << "Годин: " << hours_left << endl;
+    //cout << "Хвилин: " << final_minutes_left << endl;
+    //cout << "Секунд: " << seconds_left << endl;
+
+    //int a, b;
+    //cout << "Enter height rect: ";
+    //cin >> a;
+    //cout << "Enter wight rect: ";
+    //cin >> b;
+    //cout << "S(rect): " << a * b << "\t" << "P(rect): " << (a + b) * 2 << endl;
+
+
+    //int a, b, c;
+    //cout << "Enter first partie: ";
+    //cin >> a;
+    //cout << "Enter second partie: ";
+    //cin >> b;
+    //cout << "Enter third partie: ";
+    //cin >> c;
+    //cout << "P: " << a + b + c << endl;
+    //int p = (a + b + c) / 2;
+    //cout << "S: " << sqrt(p * (p - a) * (p - b) * (p - c)) << endl;
+
+
+    //int mass, count, price;
+    //cout << "Enter mass of one product: ";
+    //cin >> mass;
+    //cout << "Enter count: ";
+    //cin >> count;
+    //cout << "Enter price of one product: ";
+    //cin >> price;
+    //cout << "Total weight of the order: " << mass * count << endl;
+    //cout << "Total cost of the order: " << price * count << endl;
+
+    //int price, count;
+    //cout << "Enter price of the order: ";
+    //cin >> price;
+    //cout << "Enter the count of order: ";
+    //cin >> count;
+    //if (count < 5)
+    //{
+    //    cout << "Oups." << price * count << endl;
+    //}
+    //if (count > 5) {
+    //    int znuzhka = count - 5;
+    //    cout << "Price: " << ((price * count) / 100) * (10 * znuzhka) << endl;
+    //}
+
+    //int l, v;
+    //cout << "Enter distamce between two cities: ";
+    //cin >> l;
+    //cout << "Enter speed of the car: ";
+    //cin >> v;
+    //cout << "Time of the travel: " << l / v << endl;
+    //cout << "Time be left unti; the end of the day: " << (24 - 8) - (l / v) << endl;
+
+
+    //int price, count, podatok;
+    //cout << "Enter price of the one book: ";
+    //cin >> price;
+    //cout << "Enter count: ";
+    //cin >> count;
+    //cout << "Enter PDV: ";
+    //cin >> podatok;
+    //cout << "Total cost with PDV: " << (price * count) * (1 + podatok / 100) << endl;
+
+    //int v, price;
+    //cout << "Enter volume of water in lettres used per month: ";
+    //cin >> v;
+    //cout << "Enter the cost of one cubic meter of water: ";
+    //cin >> price;
+    //cout << "Total cost of water per month: " << (v / 1000) * price << endl;
+
+    //int byte, v;
+    //cout << "Enter count of byres in file: ";
+    //cin >> byte;
+    //cout << "Enter speed: ";
+    //cin >> v;
+    //cout << "Total time: " << (byte * 1024 * 1024) / v << endl;
+
+
+    //int p, counth;
+    //cout << "Enter P: ";
+    //cin >> p;
+    //cout << "Enter count of hours: ";
+    //cin >> counth;
+    //cout << "Count kB/h on day: " << (p * 1000) / counth << endl;
+    //cout << "Count kB/h on month: " << (p * 1000) / (counth*30) << endl;
+
 }
 
 
