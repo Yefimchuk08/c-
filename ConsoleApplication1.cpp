22b8e883e583de59342d21ca6bf64da41afdb716
Yefimchuk08 <dmytroyefimchuk08@gmail.com>
2024-09-15 16:00:44 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//int a = 500, sum = 0;
+	//for (int i = 0; i <= a; i++)
+	//{
+	//	sum += i;
+	//}
+	//cout << "Sum: " << sum << endl;
+
+	//int a, b;
+	//cout << "Enter num: ";
+	//cin >> a;
+	//cout << "Ente range of num: ";
+	//cin >> b;
+	//cout << pow(a, b) << endl;
+
+	//int start = 1, finish = 1000, count = 0 ,sum = 0;
+	//for (int i = start; i <= finish; i++)
+	//{
+	//	sum += i;
+	//	count++;
+	//}
+	//cout << "Ser Ar: " << (float)sum / count << endl;
+
+	//int a, multiply = 1;
+	//cout << "Enter num 1 <= num <= 20: ";
+	//cin >> a;
+	//if (1 <= a <= 20)
+	//{
+	//	for (int i = a; i <= 20; i++)
+	//	{
+	//		multiply *= i;
+	//	}
+	//	cout << multiply << endl;
+	//}
+
+	//int k;
+	//cout << "Enter numb: ";
+	//cin >> k;
+	//for (int i = k; i <= k; i++)
+	//{
+	//	for (int j = 1; j <= 10; j++)
+	//	{
+	//		cout <<k <<" * "<< j << " = " << j * i << endl;
+	//	}
+	//}
+
+	//int count = 0;
+
+	//for (int i = 100; i <= 999; i++) {
+	//	int hundreds = i / 100;       
+	//	int tens = (i / 10) % 10;     
+	//	int ones = i % 10;            
+
+	//	
+	//	if (hundreds == tens || hundreds == ones || tens == ones) {
+	//		count++;
+	//	}
+	//}
+
+	//cout << "Number of integers with two identical digits: " << count << endl;
+
+
+	//int count = 0;
+
+	//for (int i = 100; i <= 999; i++) {
+	//	int hundreds = i / 100;
+	//	int tens = (i / 10) % 10;
+	//	int ones = i % 10;
+
+
+	//	if (hundreds != tens || hundreds != ones || tens != ones) {
+	//		count++;
+	//	}
+	//}
+
+	//cout << "Number of integers with тще identical digits: " << count << endl;
+
+
+	//int number, result = 0;
+	//int factor = 1;
+	//cout << "Enter an integer: ";
+	//cin >> number;
+	//int absNumber = (number < 0) ? -number : number;
+	//while (absNumber > 0) {
+	//	int digit = absNumber % 10; 
+	//	absNumber /= 10;            
+
+	//	if (digit != 3 && digit != 6) {
+	//		result += digit * factor;
+	//		factor *= 10; 
+	//	}
+	//}
+	//if (result == 0 && number != 0) {
+	//	result = 0;
+	//}
+	//if (number < 0) {
+	//	result = -result;
+	//}
+	//cout << "Result: " << result << endl;
+
+	//int A;
+	//cout << "¬вед≥ть число A: ";
+	//cin >> A;
+
+	//for (int B = 1; B <= sqrt(A); ++B) {
+	//	if (A % (B * B) == 0 && A % (B * B * B) != 0) {
+	//		cout << B << endl;
+	//	}
+	//}
+
+	//int a;
+
+	//cout << "Enter an integer : ";
+	//cin >> a;
+
+	//int originalNumber = a;
+	//int sumOfDigits = 0;
+	//while (a != 0) {
+	//	sumOfDigits += a % 10;
+	//	a /= 10;
+	//}
+	//int cubeOfSum = sumOfDigits * sumOfDigits * sumOfDigits;
+	//int squareOfNumber = originalNumber * originalNumber;
+	//if (cubeOfSum == squareOfNumber) {
+	//	cout << "The cube of the sum of digits equals the square of the number." << endl;
+	//}
+	//else {
+	//	cout << "The cube of the sum of digits does not equal the square of the number." << endl;
+	//}
+
+	//int a;
+	//cout << "Enter numb: ";
+	//cin >> a;
+	//for (int i = 1; i <= a; i++)
+	//{
+	//	if (a % i == 0) {
+	//		cout << i << endl;
+	//	}
+	//	else {
+	//		continue;
+	//	}
+	//}
+
+	//int a, b;
+	//cout << "Enter first num: ";
+	//cin >> a;
+	//cout << "Enter second num: ";
+	//cin >> b;
+	//if (a < b) {
+	//	for (int i = 1; i <= b; i++)
+	//	{
+	//		if (a % i == 0 && b % i == 0)
+	//		{
+	//			cout << i << endl;
+	//		}
+	//		else {
+	//			continue;
+	//		}
+	//	}
+	//}
+	//if (a > b) {
+	//	for (int i = 1; i <= a; i++)
+	//	{
+	//		if (a % i == 0 && b % i == 0) {
+	//			cout << i << endl;
+	//		}
+	//		else {
+	//			continue;
+	//		}
+	//	}
+	//}
+
+}
+
+
+
---
+++ homework2.2.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //char ch;
+    //cout << "Enter symbol: ";
+    //cin >> ch;
+    //if (isalpha(ch))
+    //{
+    //    cout << "Is letter" << endl;
+    //}
+    //else if (isdigit(ch))
+    //{
+    //    cout << "Is num" << endl;
+    //}
+    //else if (ispunct(ch))
+    //{
+    //    cout << "Is mark" << endl;
+    //}
+    //else {
+    //    cout << "Is other symbol" << endl;
+    //}
+
+
+
+    //int  choise, operathor, time;
+    //while (true)
+    //{
+    //    cout << "[1] - Lifecell; \n[2] - Kyivstar; \n[3] - Vodaphone;\n Whar your operathor: ";
+    //    cin >> operathor;
+    //    switch (operathor)
+    //    {
+    //    case 1:
+    //        cout << "[1] - Lifecell; Price: free \n[2] - Kyivstar; Price: $1/h \n[3] - Vodaphone; Price: $2/h \n Enter operathor to your call: ";
+    //        cin >> choise;
+    //        switch (choise)
+    //        {
+    //        case 1:
+    //            cout << "Enter call time: ";
+    //            cin >> time;
+    //            cout << "You must pay: $" << time << endl;
+    //        case 2:
+    //            cout << "Enter call time: ";
+    //            cin >> time;
+    //            cout << "You must pay: $" << time * 2 << endl;
+    //        case 3:
+    //            cout << "Enter call time: ";
+    //            cin >> time;
+    //            cout << "You must pay: $" << time * 2 << endl;
+    //        default:
+    //            break;
+    //        }
+
+    //    case 2:
+    //        cout << "[1] - Lifecell; Price: $1 \n[2] - Kyivstar; Price: free \n[3] - Vodaphone; Price: $2/h \n Enter operathor to your call: ";
+    //        cin >> choise;
+    //        switch (choise)
+    //        {
+    //        case 1:
+    //            cout << "Enter call time: ";
+    //            cin >> time;
+    //            cout << "You must pay: $" << time << endl;
+    //        case 2:
+    //            cout << "Enter call time: ";
+    //            cin >> time;
+    //            cout << "You must pay: $" << time * 2 << endl;
+    //        case 3:
+    //            cout << "Enter call time: ";
+    //            cin >> time;
+    //            cout << "You must pay: $" << time * 2 << endl;
+    //        default:
+    //            break;
+    //        }
+    //    case 3:
+    //        cout << "[1] - Lifecell; Price: $1 \n[2] - Kyivstar; Price: $1/h \n[3] - Vodaphone; Price: free \n Enter operathor to your call: ";
+    //        cin >> choise;
+    //        switch (choise)
+    //        {
+    //        case 1:
+    //            cout << "Enter call time: ";
+    //            cin >> time;
+    //            cout << "You must pay: $" << time << endl;
+    //        case 2:
+    //            cout << "Enter call time: ";
+    //            cin >> time;
+    //            cout << "You must pay: $" << time * 2 << endl;
+    //        case 3:
+    //            cout << "Enter call time: ";
+    //            cin >> time;
+    //            cout << "You must pay: $" << time * 2 << endl;
+    //        default:
+    //            break;
+    //        }
+    //    default:
+    //        break;
+    //    }
+    //}
+
+    //while (true)
+    //{
+    //    int choise;
+    //    cout << "[1] - the user enters the desired Wasi income and the amount delays, count how many lines of code he needs write;\n [2] - the user enters the number of lines of code written Vasey and desired amount of salary.Count how much sometimes Vasya may be late;\n [3] -  the user enters the number of lines of code and the number delays, to determine how much money Vasa will be paid and whether they will pay at all.\n Whar your choise: ";
+    //    cin >> choise;
+    //    switch (choise)
+    //    {
+    //    case 1: 
+    //        int dohid, count;
+    //        cout << "Enter desired income: ";
+    //        cin >> dohid;
+    //        cout << "Enter the number of delays: ";
+    //        cin >> count;
+    //        int total_fine = (count / 3) * 20;
+    //        int adjusted_income = dohid + total_fine;
+    //        int lines_of_code = (adjusted_income * 100) / 50;
+    //        cout << "Vasya must write " << lines_of_code << " lines of code." << endl;
+    //    case 2:
+    //        int count_of_lines, zp;
+    //        cout << "Enter count of lines: ";
+    //        cin >> count_of_lines;
+    //        cout << "Enter desired salary (zp): ";
+    //        cin >> zp;
+    //        int money_with_lines = (count_of_lines / 100) * 50;
+    //        int zalushok = money_with_lines - zp;
+    //        if (zalushok >= 0) {
+    //            cout << "Vasya can delay 0 times, as his salary is already sufficient." << endl;
+    //            return 0;
+    //        }
+    //        int count_of_delays = 0;
+    //        while (zalushok < 0) {
+    //            count_of_delays++;
+    //            if (count_of_delays % 3 == 0) {
+    //                zalushok += 20;
+    //            }
+    //        }
+    //        cout << "Vasya can delay " << count_of_delays << " times." << endl;
+    //    case 3:
+
+    //        int count_of_liness, count_of_delayss;
+    //        cout << "Enter count of lines: ";
+    //        cin >> count_of_liness;
+    //        cout << "Enter count of delays: ";
+    //        cin >> count_of_delayss;
+    //        int salary = (count_of_liness / 100) * 50;
+    //        int sum_with_delays = (count_of_delayss / 3) * 20;
+    //        int minus = salary - sum_with_delays;
+    //        if (minus < 0)
+    //        {
+    //            cout << "Vasya don`t have a cash" << endl;
+    //        }
+    //        else {
+    //            cout << "Vasya take a: " << minus << " cash" << endl;
+    //        }
+    //    default:
+    //        cout << "Invalid choise!!!" << endl;
+    //        break;
+    //    }
+    //}
+}
