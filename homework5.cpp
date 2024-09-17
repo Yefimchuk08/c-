ba86b40020967aac88315bb60d72915c80e63298
Yefimchuk08 <dmytroyefimchuk08@gmail.com>
2024-09-17 14:31:30 +0300

    Add files via upload

---
+++ homework5.cpp
@@ -0,0 +1,175 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main()
+{
+    //const int a = 5;
+    //const int b = 5;
+    //int array[a][b];
+    //int one;
+    //cout << "Enter first num: ";
+    //cin >> one;
+    //array[0][0] = one;
+    //array[0][1] = one *= 2;
+    //array[0][2] = one *= 2;
+    //array[0][3] = one *= 2;
+    //array[0][4] = one *= 2;
+    //array[1][0] = one *= 2;
+    //array[1][1] = one *= 2;
+    //array[1][2] = one *= 2;
+    //array[1][3] = one *= 2;
+    //array[1][4] = one *= 2;
+    //array[2][0] = one *= 2;
+    //array[2][1] = one *= 2;
+    //array[2][2] = one *= 2;
+    //array[2][3] = one *= 2;
+    //array[2][4] = one *= 2;
+    //array[3][0] = one *= 2;
+    //array[3][1] = one *= 2;
+    //array[3][2] = one *= 2;
+    //array[3][3] = one *= 2;
+    //array[3][4] = one *= 2;
+    //array[4][0] = one *= 2;
+    //array[4][1] = one *= 2;
+    //array[4][2] = one *= 2;
+    //array[4][3] = one *= 2;
+    //array[4][4] = one *= 2;
+
+    //for (int i = 0; i < a; i++)
+    //{
+    //    for (int j = 0; j < b; j++)
+    //    {
+    //        cout << array[i][j] << endl;
+    //    }
+    //}
+
+
+    //const int a = 5;
+    //const int b = 5;
+    //int array[a][b];
+    //int one;
+    //cout << "Enter first num: ";
+    //cin >> one;
+    //array[0][0] = one;
+    //array[0][1] = one += 1;
+    //array[0][2] = one += 1;
+    //array[0][3] = one += 1;
+    //array[0][4] = one += 1;
+    //array[1][0] = one += 1;
+    //array[1][1] = one += 1;
+    //array[1][2] = one += 1;
+    //array[1][3] = one += 1;
+    //array[1][4] = one += 1;
+    //array[2][0] = one += 1;
+    //array[2][1] = one += 1;
+    //array[2][2] = one += 1;
+    //array[2][3] = one += 1;
+    //array[2][4] = one += 1;
+    //array[3][0] = one += 1;
+    //array[3][1] = one += 1;
+    //array[3][2] = one += 1;
+    //array[3][3] = one += 1;
+    //array[3][4] = one += 1;
+    //array[4][0] = one += 1;
+    //array[4][1] = one += 1;
+    //array[4][2] = one += 1;
+    //array[4][3] = one += 1;
+    //array[4][4] = one += 1;
+
+    //for (int i = 0; i < a; i++)
+    //{
+    //    for (int j = 0; j < b; j++)
+    //    {
+    //        cout << array[i][j] << endl;
+    //    }
+    //}
+
+
+    //srand(time(NULL));
+
+    //int rows, cols, shifts;
+    //char direction;
+    //cout << "Enter count of rows: ";
+    //cin >> rows;
+    //cout << "Enter count of colums: ";
+    //cin >> cols;
+
+
+    //int arr[100][100]; 
+    //for (int i = 0; i < rows; ++i) {
+    //    for (int j = 0; j < cols; ++j) {
+    //        arr[i][j] = rand() % 100; 
+    //    }
+    //}
+
+    //
+    //cout << "Start" << endl;
+    //for (int i = 0; i < rows; ++i) {
+    //    for (int j = 0; j < cols; ++j) {
+    //        cout << arr[i][j] << " ";
+    //    }
+    //    cout << endl;
+    //}
+    //cout << "Enter (a - left, d - right, w - up, s - down): ";
+    //cin >> direction;
+
+    //for (int s = 0; s < 3; ++s) {
+    //    if (direction == 'a') { 
+    //        for (int i = 0; i < rows; ++i) {
+    //            int first = arr[i][0];
+    //            for (int j = 0; j < cols - 1; ++j) {
+    //                arr[i][j] = arr[i][j + 1];
+    //            }
+    //            arr[i][cols - 1] = first;
+    //        }
+    //    }
+    //    else if (direction == 'd') { 
+    //        for (int i = 0; i < rows; ++i) {
+    //            int last = arr[i][cols - 1];
+    //            for (int j = cols - 1; j > 0; --j) {
+    //                arr[i][j] = arr[i][j - 1];
+    //            }
+    //            arr[i][0] = last;
+    //        }
+    //    }
+    //    else if (direction == 'w') { 
+    //        int firstRow[100];
+    //        for (int j = 0; j < cols; ++j) {
+    //            firstRow[j] = arr[0][j];
+    //        }
+    //        for (int i = 0; i < rows - 1; ++i) {
+    //            for (int j = 0; j < cols; ++j) {
+    //                arr[i][j] = arr[i + 1][j];
+    //            }
+    //        }
+    //        for (int j = 0; j < cols; ++j) {
+    //            arr[rows - 1][j] = firstRow[j];
+    //        }
+    //    }
+    //    else if (direction == 'd') { 
+    //        int lastRow[100];
+    //        for (int j = 0; j < cols; ++j) {
+    //            lastRow[j] = arr[rows - 1][j];
+    //        }
+    //        for (int i = rows - 1; i > 0; --i) {
+    //            for (int j = 0; j < cols; ++j) {
+    //                arr[i][j] = arr[i - 1][j];
+    //            }
+    //        }
+    //        for (int j = 0; j < cols; ++j) {
+    //            arr[0][j] = lastRow[j];
+    //        }
+    //    }
+    //}
+
+
+    //cout << "Finish:" << endl;
+    //for (int i = 0; i < rows; ++i) {
+    //    for (int j = 0; j < cols; ++j) {
+    //        cout << arr[i][j] << " ";
+    //    }
+    //    cout << endl;
+    //}
+}
